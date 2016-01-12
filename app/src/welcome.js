36c3533f09e9718db0f7704120de6b266d53fcad
John Chynoweth <jchynowe1@yahoo.com>
2016-01-12 13:13:35 -0500

    unit tests

--- app/src/welcome.js
+++ app/src/welcome.js
@@ -7,11 +7,11 @@ export class Welcome {
     constructor(httpClient) {
         this.httpClient = httpClient;
         this.zip = '22207';
-        this.speed = '';
-        this.direction = 'SE';
-        this.temp = '60';
-        this.heading = 'Forecast';
-        this.forecast;
+      //  this.speed = '';
+      //  this.direction = 'SE';
+      //  this.temp = '60';
+       this.heading = 'Forecast';
+       this.forecast = {currently : 'fooo'};
     }
 
     activate() {
@@ -19,7 +19,8 @@ export class Welcome {
         return this.httpClient.fetch("api/forecast")
         .then(response => response.json())
         .then(data => {
-            this.forecast = data;
+          this.forecast = data.currently;
+         //  this.speed = current.windSpeed;
         });
     }
 }
--- app/test/unit/welcome.spec.js
+++ app/test/unit/welcome.spec.js
@@ -8,48 +8,31 @@ class HttpStub {
         this.url = url;
         return new Promise((resolve) => {
             resolve({ json: () => response });
-
         });
     }
-
     configure(func) {
     }
 }
 
 describe('the Welcome module', () => {
-    it('sets fetch response to forecast', (done) => {
-        var http = new HttpStub();
-        expect(http).toBeDefined();
-       var sut = new Welcome(http);
-        expect(sut).toBeDefined();
-
-
-        var forecastStubs = [1];
-        var forecastFake = [2];
-
-        http.forecastStub = forecastStubs;
-        
-
-        sut.activate().then(() => {
-            expect(sut.forecast).toBe(forecastStubs);
-            expect(sut.forecast).not.toBe(forecastFake);
-            done();
-        });
+    var data, forecast, http;
     
-    // var res = sut.activate();
-    // expect(res).toBeDefined();
-        // expect(sut).toBeDefined();
-        //expect(1).toBe(1);
-             
-    });
-    
-    it('Get sample forecst', () => {
-        var forecast = new Forecast();
-        
-        var data = forecast.getSample();
-         expect(data).toBeDefined();
-         
+     beforeEach(() => {
+          forecast = new Forecast();
+          data = forecast.getSample();
+           http = new HttpStub();
+           http.forecastStub = data;
      });
+     
+    it('sets fetch response to forecast', (done) => {
+       var sut = new Welcome(http); 
+       
+        sut.activate().then(() => {
+            expect(sut.forecast).toBe(data.currently);
+            //expect(sut.forecast).toBeDefined();
+             done();
+        });           
+    });
     
      
 });
