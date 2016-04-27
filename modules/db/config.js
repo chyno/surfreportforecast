11a64f8cec255dda0ecfe5bdbcce4222050e9f56
John Chynoweth <jchynowe1@yahoo.com>
2016-04-27 12:35:22 -0400

    create data filter

--- modules/db/config.js
+++ modules/db/config.js
@@ -5,5 +5,5 @@ config.authKey = process.env.AUTH_KEY || "DSILv0gPPYt6O9mDNN0eX/sFf6ZTA13OaI9QYX
 config.databaseId = "chynosurf";
 config.zipCollectionId = "zips";
 config.userCollectionId = "user";
-
+config.forcastKey = "0f9877d63b94697f985124d9cbb9c6cb";
 module.exports = config;
--- test.js
+++ test.js
@@ -1,33 +1,36 @@
-//var R = require('ramda-maybe');
+var R = require('ramda');
 //var M = require('ramda-fantasy').Maybe;
+var frc = require("./app/test/fake/forecast.js");
 var config = require('./modules/db/config');
 var docdbUtils = require('./docdbUtils');
 var http = require('requestify');
 var key = "0f9877d63b94697f985124d9cbb9c6cb";
 
+/*
 function showForcastList(location) {
+     console.log(location);
    var lat = location.latitude;
     var long = location.longitude;
     var path = "/forecast/" + key + "/" + lat + "," + long;
     var host = 'https://api.forecast.io';
     
     return new Promise((resolve, reject) => { 
-        
+         var fullpath = host + path;
+          console.log('Path: ' + fullpath);
         http.get(host + path).then((response) => {
              resolve(response.getBody()); 
           }, (reason) => {
+                console.log('rejecting ..');
              reject(reason); 
           });
           
           
     });
         
-}
-
- 
+} 
 
 var client = docdbUtils.getZips(config, '22207').then((zips) => {
-    showForcastList(zips).then((forcast) =>{
+    showForcastList(zips[0]).then((forcast) =>{
         console.log(forcast);
     }, (reason) =>{
         console.log(reason);
@@ -36,5 +39,22 @@ var client = docdbUtils.getZips(config, '22207').then((zips) => {
 
 
 
+*/
+
+var currentlyPath = R.lensPath(['currently']);
+var forecastPath = R.lensPath(['daily', 'data']);
+//var vm = {currently : '', forecast: ''};
+var data = frc.getSample();
+var createVM = R.curry( (vm,d) =>
+{
+    vm.forecast = R.view(forecastPath, d);
+     vm.currently = R.view(currentlyPath, d);
+    return vm;
+}
+);
 
 
+//var rv = R.view(dailyPath);
+
+console.log(createVM({},data));
+//console.log(R.view(currentlyPath,data));
