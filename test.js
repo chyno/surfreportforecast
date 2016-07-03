e8a3bf473f2d108c8f33278a2f4c13eed861287b
John Chynoweth <jchynowe1@yahoo.com>
2016-07-02 22:36:03 -0400

    can get locations

--- test.js
+++ test.js
@@ -1,65 +1,15 @@
 'use strict'
 var R = require('ramda');
 var moment = require('moment');
-//var M = require('ramda-fantasy').Maybe;
-
 var docdbUtils = require('./lib/docdbUtils');
 
-var fakeRes = {
-    json(vm)
-    {
-       // var result = vm.forecast;
-        //console.log(vm.forecast[0]);//result.daily.data[0].toString());
-        console.log(vm.forecast[0]);
-    }
-}
-
-
-var createVMPromise = R.pipe(docdbUtils.createVM, (val) => Promise.resolve(val));
-var forcastCalc = R.composeP(createVMPromise,docdbUtils.showForcastByLongLat, docdbUtils.getLatLongByZip);
-var renderParamRequest = R.curry((fun, req, res) => {
-        
-   if(!req.params.id)
-        { throw ("no parameter");}
-        
-         fun(req.params.id).then((x) => {
-            var result = docdbUtils.createVM({},x);
-            console.log(x);  
-        });
-  });
+ 
+ // var forcastCalc = R.composeP(createVMPromise, docdbUtils.showForcastByLongLat, R.head,  docdbUtils.getLatLongByZip);
 
  
- 
- var getStateZips = (state) => {
-    
-    return new Promise((resolve, reject) =>
-    {
-        resolve(
-    [
-        {zip: '11111111', 
-        city: 'Honolulu' },
-        {zip: '22222222', 
-        city: 'Paeia' },
-        {zip: '555555', 
-        city: 'SPreksville' }
-    ]);
-    }    
-    );
-}
+docdbUtils.getLatLongByZip('22207').then (res => {
+    var res2 = R.head(res);
+    console.log(res2);
+})
 
  
-
-forcastCalc('22207').then(x => {
-   
-  console.log('VM: ' +x.city);  
-} );
-
-//var formatdate = cmoment("MMM Do YY");
-
-//var a = cmoment('1462852800');
-
-//router.get('/api/zip/:id', renderParamRequest(forcastCalc));
-//renderParamRequest(forcastCalc, {params: {id: '22207'}}, fakeRes);
-//console.log(getStateZips);
-//getStateZips('1').then((res) => {console.log(res);});
-  
